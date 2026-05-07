a428f96fafebfb8d3d4a97daa12a9fcd763de10f
Your Name <your@email.com>
2026-05-07 16:46:41 -0500

    cse 5 hour final by james mcmahon-lee

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 # ============================================================
 # Python Final Project 2026
-# Name: 
-# Date: 
-# Project Title: 
-# Description: (Write 1-2 sentences explaining what your program does)
+# Name: james mcmahon lee
+# Date: 5/7/2026
+# Project Title: garde calculator
+# Description: this program will calulate what a student got in leter grade. after it tell them their grade it will then ask what of assiment it is and tell them the impact.
 # ============================================================
 
 
 # ---- SECTION 1: Setup / Variables ----
 # Create your starting variables here.
 # Example: player_name = ""
-
+user = input("whats your name ")
 
 
 # ---- SECTION 2: Welcome Message ----
 # Greet the user and explain what your program does.
 
 print("Welcome!")
-print("----------------------------")
+print("are you ready to calculate your grade")
 
 
 
@@ -25,11 +25,17 @@ print("----------------------------")
 # Use input() to ask the user for information.
 # Remember: input() always returns a string.
 # Use int() or float() if you need a number.
+numerQestions = int(input(print("how many questions are there")))
+questionsCorrect = int(input(print("how many questions did you get correct")))
+assimentType = str(input(print("is this a quiz,test or homework")))
 
 # Example:
 # player_name = input("What is your name? ")
 # score = int(input("Enter a number: "))
 
+# this is the code for the calculatons
+grade = int(questionsCorrect) / int(numerQestions)
+print (grade)
 
 
 # ---- SECTION 4: Logic (if / elif / else) ----
@@ -43,10 +49,31 @@ print("----------------------------")
 # else:
 #     print("Keep practicing!")
 
+if grade >= 0.9 :
+    print("you got an A great job") 
+elif grade >= 0.8: 
+    print("you got a B your doing great")
+elif grade >= 0.7: 
+    print ("you got a C keep trying harder every day")
+elif grade >= 0.6:
+     print ("you got a D dont give up ")
+elif grade < .6:
+    print("you failed")
+else:
+    print("you failed")
+
+if assimentType == str("test"):
+    print('this will have a major impact')
+elif assimentType == str("quiz"):
+    print('this will have a medium impact')
+else:
+    print("this will have a minor impact")
+
+
 
 
 # ---- SECTION 5: Final Output ----
 # Print a final message, result, or summary to the user.
 
-print("----------------------------")
+print("i hope your happy with what grade you got and wish you luck until next time")
 print("Thanks for using my program!")
